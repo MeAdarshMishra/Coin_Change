4f72d00329018af839191cad23bdb7a005eabd6e
MeAdarshMishra <adarsh.mishra_cs19@gla.ac.in>
2022-01-18 22:22:53 +0530

    Updated

--- src/Coin_Change.java
+++ src/Coin_Change.java
@@ -9,22 +9,33 @@ public class Coin_Change {
         for (int i = 0; i < coin.length; i++) {
             coin[i]=sc.nextInt();
         }
-        System.out.println(numberOfWays(coin,amount,0));
+        int [][] dp=new int [amount+1][n];
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[i].length; j++) {
+                dp[i][j]=-1;
+            }
+
+            
+        }
+        System.out.println(numberOfWays(coin,amount,0,dp));
        // System.out.println(coin,amount,0);
     }
-    public static int numberOfWays(int[] coin,int amount,int i) {
+    public static int numberOfWays(int[] coin,int amount,int i,int [][]dp) {
         if (amount==0){
             return 1;
         }
         if (i==coin.length){
             return 0;
         }
+        if (dp[amount][i]!=-1 ){
+            return dp[amount][i];
+        }
         int include=0;
         int exclude=0;
         if (amount>=coin[i]){
-            include=numberOfWays(coin , amount-coin[i],i);
+            include=numberOfWays(coin , amount-coin[i],i,dp);
         }
-        exclude=numberOfWays(coin, amount, i+1);
-        return include+exclude;
+        exclude=numberOfWays(coin, amount, i+1,dp);
+        return dp[amount][i]=include+exclude;
     }
 }
